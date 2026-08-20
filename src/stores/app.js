718e7771cecf9e63f20ba8028a01ac08f9ec9653
Farghana Hussain <farghana@gmail.com>
2026-08-20 01:03:02 -0400

    feat: add Pinia app store foundation

---
+++ src/stores/app.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useAppStore = defineStore('app', () => {
+  const sidebarOpen = ref(false)
+
+  function toggleSidebar() {
+    sidebarOpen.value = !sidebarOpen.value
+  }
+
+  function closeSidebar() {
+    sidebarOpen.value = false
+  }
+
+  return {
+    sidebarOpen,
+    toggleSidebar,
+    closeSidebar,
+  }
+})
