ff453a81320610ea92ac51e85c16d7fd45fd5fef
Farghana Hussain <farghana.hussain@itconquest.com>
2026-08-20 01:18:47 -0400

    chore: initialize shadcn-vue components

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+
+  test: {
+    environment: 'jsdom',
   },
 })
